b8e125082350cbd0ce61e000074bcdce61f7cf6f
Staska45 <55595953+Staska45@users.noreply.github.com>
2019-09-22 17:42:24 +0300

    Configuration file
    Database configuration file for database and 2 tables

---
+++ Config.php
@@ -0,0 +1,8 @@
+   <?php
+     $servername = "localhost";
+     $username = "user";
+     $password = "password";
+     $dbname = "dbname";
+     $DB_table_R = 'DB_table_R';
+     $DB_table_U = 'DB_table_U';
+     ?>
